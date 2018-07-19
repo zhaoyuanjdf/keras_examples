d9e6d37924639ed7514e2fb3765db92af28dd14f
yh <545239806@qq.com>
2018-07-20 05:01:27 +0800

    x

--- test01/ResNet50.py
+++ test01/ResNet50.py
@@ -5,14 +5,19 @@ import numpy as np
 
 model = ResNet50(weights='imagenet')
 
-img_path = '/Users/yh/PycharmProjects/keras_examples/IMG/2.jpg'
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
+Predicted = []
+for i in range(2):
+    img_path = "/Users/yh/PycharmProjects/keras_examples/IMG/%d.jpg"  %(i+1)
+    # model_save_path = now.strftime("%y%m%d%H") + "customer_loss_train"
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
 
-preds = model.predict(x)
-# decode the results into a list of tuples (class, description, probability)
-# (one such list for each sample in the batch)
-print('Predicted:', decode_predictions(preds, top=3))
-# Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
+    preds = model.predict(x)
+    # decode the results into a list of tuples (class, description, probability)
+    # (one such list for each sample in the batch)
+    # print('Predicted:', decode_predictions(preds, top=3))
+    Predicted.append(decode_predictions(preds, top=1))
+    # Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
+print(Predicted)
